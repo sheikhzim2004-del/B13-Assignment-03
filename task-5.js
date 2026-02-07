b4479c06bdbe264a8a09cd0ace97ff8f0fac3fb2
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-02-07 15:55:20 +0600

    05 no task complete

--- task-5.js
+++ task-5.js
@@ -2,5 +2,21 @@
 
 /*function signature/sample */
 function  analyzeText(str) {
-          // You have to write your code here
+  if(typeof str !== 'string'){
+    return 'Invalid';
+  }
+  const words = str.split(' ');
+  let longword = words[0];
+  for (let word of words){
+    if(word.length > longword.length){
+      longword = word;
+    }
+  }
+  const token = words.join('').length;
+   return {
+      longword,
+      token,
+    }
 }
+const result = analyzeText("Keep coding keep shining");
+console.log(result);
