f7d770f70fd9f5a0db9feb8911711f887f1f0d99
sheikhzim2004-del <sheikhzim2004@gmail.com>
2026-02-07 12:41:46 +0600

    task 03 complete

--- task-3.js
+++ task-3.js
@@ -2,7 +2,22 @@
 
 /*function signature/sample */
 function finalScore (omr) {
-          //write your code here
-}
+
+  const {right, wrong, skip} = omr;
+  const total = right + wrong + skip;
+  if(total !== 100){
+    return 'Invalid'
+  }
+  if(typeof right !== 'number' || typeof wrong !== 'number' || typeof skip !== 'number'){
+    return 'Invalid';
+  }
+  const cutMark = wrong * 0.5
+  const totalScore = Math.round(right - cutMark);
+  return totalScore;
+  }
+
+const omr = { right: 67, wrong: 23, skip: 10 };
+const result = finalScore(omr);
+console.log(result);
 
 
